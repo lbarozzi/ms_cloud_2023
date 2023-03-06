0db0331916c04ace933966ae081a64e2010175e0
Leonardo Barozzi <leonardo.barozzi@mail.com>
2023-03-06 10:03:40 +0100

    View List

---
+++ Day7Lab1/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Runtime.CompilerServices;
+
+namespace Day7Lab1.Pages {
+    public class IndexModel : PageModel {
+        private readonly ILogger<IndexModel> _logger;
+        public ToDoList _list;
+
+        public IndexModel(ILogger<IndexModel> logger,ToDoList lista) {
+            _logger = logger;
+            _list = lista;
+        }
+
+        public void OnGet() {
+            if(_list.Count==0) {
+                _list.PopulateList(10);
+            }
+        }
+    }
+}
---
+++ Day7Lab1/ToDoModel.cs
@@ -0,0 +1,39 @@
+﻿namespace Day7Lab1 {
+    public class ToDoModel {
+        public enum ToDoPriority {
+            Low,Normal,High,VeryHigh
+        }
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public ToDoPriority PriorityLevel { get; set; }
+        public bool done { get; set; }
+        public ToDoModel() { 
+            this.Description = string.Empty;
+        }
+    }
+    public class ToDoList: List<ToDoModel> { 
+        public ToDoList() { }
+        public void PopulateList(int records=5) {
+            string[] elenco =
+            {
+                "Pulire Lettiera",
+                "Prepare letto",
+                "Telefonare Antanti",
+                "Tagliando Macchina",
+                "Sbiriguda"
+            };
+            Random r = new Random();
+            for(int i=0; i < records; i++) {
+                this.Add(new ToDoModel() {  
+                    ID = i ,
+                    Description = $"{elenco[r.Next(elenco.Length)]} {i}",
+                    DueDate = DateTime.Now,
+                    done = false    ,
+                    PriorityLevel=(ToDoModel.ToDoPriority)r.Next(
+                        Enum.GetValues(typeof(ToDoModel.ToDoPriority)).Length ) ,
+                });
+            }
+        }
+    }
+}
