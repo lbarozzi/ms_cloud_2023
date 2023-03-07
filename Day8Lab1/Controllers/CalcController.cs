9b77e5ce716cb3566e9ac6695e8ae21f29c0e8bf
Leonardo Barozzi <leonardo.barozzi@mail.com>
2023-03-07 10:25:28 +0100

    Init project

---
+++ Day8Lab1/Controllers/CalcController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Day8Lab1.Controllers {
+    public class CalcController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
