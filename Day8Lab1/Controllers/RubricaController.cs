388c4e4ded0b32d445c7cbcca83b9c8a9eecec7a
Leonardo Barozzi <leonardo.barozzi@mail.com>
2023-03-07 16:11:48 +0100

    CRUD

--- Day8Lab1/Controllers/RubricaController.cs
+++ Day8Lab1/Controllers/RubricaController.cs
@@ -20,7 +20,57 @@ namespace Day8Lab1.Controllers {
         }
 
         public IActionResult Index() {
-            return View();
+            return View(_rub);
         }
+
+        public IActionResult Update(int? antani) {
+            if(antani== null || antani >= _rub.Count)
+                return RedirectToAction("index");
+            ViewData["action"] = "Update";
+            return View("/Views/Rubrica/Anagraphic.cshtml", _rub[(int)antani]);
+        }
+        [HttpPost]
+        public IActionResult Update(int? antani, [FromForm] Anagraphic updated) {
+            if (antani == null || antani >= _rub.Count || updated==null)
+                return RedirectToAction("index");
+
+            try {
+                _rub[(int)antani] = updated;
+            }catch(Exception ex) {
+                //LOG THIS SHIT, HOLY SHIT
+            }
+
+            return RedirectToAction("index");
+        }
+
+        //TODO: DELETE!! 
+        public IActionResult Delete(int? tokill) {
+            if (tokill == null || tokill >= _rub.Count) {
+                return RedirectToAction("index"); 
+            }
+            ViewData["action"] = "** DELETE **";
+            return View("/Views/Rubrica/Anagraphic.cshtml", _rub[(int)tokill]);
+        }
+        [HttpPost]
+        public IActionResult Delete(int? tokill, [FromForm] Anagraphic deleted) {
+            if (tokill == null || tokill >= _rub.Count||deleted==null) {
+                return RedirectToAction("index");
+            }
+            _rub.RemoveAt((int)tokill);
+            return RedirectToAction("index");
+
+        }
+
+        [HttpGet]
+        public IActionResult New() {
+            ViewData["action"] = "Add New";
+            return View("/Views/Rubrica/Anagraphic.cshtml", new Anagraphic());
+        }
+
+        [HttpPost]
+        public IActionResult New([FromForm] Anagraphic Nuova) {
+            _rub.Add(Nuova);
+            return RedirectToAction("index");
+        } 
     }
 }
--- Day8Lab1/Models/Rubrica.cs
+++ Day8Lab1/Models/Rubrica.cs
@@ -1,10 +1,14 @@
-﻿namespace Day8Lab1.Models {
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Day8Lab1.Models {
     public class Anagraphic {
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
         public string Email { get; set; }
 
+        
         public string FullName { get { return $"{LastName} {FirstName}"; } }
         public Anagraphic() { 
             FirstName = string.Empty;
