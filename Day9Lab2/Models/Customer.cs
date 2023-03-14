a3ff3a66632f6203ab7aa497e187a9afbfb6739d
Leonardo Barozzi <leonardo.barozzi@mail.com>
2023-03-14 12:51:47 +0100

    Base Model

---
+++ Day9Lab2/Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿namespace Day9Lab2.Models {
+    public class Customer {
+        public int ID { get; set; }
+        public string VAT { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsBlacklisted { get; set; }
+        public string PictureName { get; set; }
+
+        public Customer() {
+            VAT = FullName = Email = Phone = Address = City = PostalCode = Country = string.Empty;
+            IsActive = true;
+            IsBlacklisted=false;
+        }
+    }
+}
