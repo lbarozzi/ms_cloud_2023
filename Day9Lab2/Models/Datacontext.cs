e152baf246eff18122f2743e60540aa27830ea4d
Leonardo Barozzi <leonardo.barozzi@mail.com>
2023-03-14 14:17:12 +0100

    Datacontext

---
+++ Day9Lab2/Models/Datacontext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Day9Lab2.Models
+{
+    public class Datacontext: Microsoft.EntityFrameworkCore.DbContext
+    {
+        public Datacontext(DbContextOptions<Datacontext> options) : base(options) { }   
+        public virtual DbSet<Customer> Customers { get; set; }  
+    }
+}
