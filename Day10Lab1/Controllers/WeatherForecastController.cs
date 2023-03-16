9608372d704abedc31eb56f7a1183a35a57ef335
Leonardo Barozzi <leonardo.barozzi@mail.com>
2023-03-16 10:22:25 +0100

    Demo API app

---
+++ Day10Lab1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,38 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Day10Lab1.Controllers {
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger) {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get() {
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+        //
+        [HttpPost(Name = "GetMultiWeatherForecast")]
+        public IEnumerable<WeatherForecast> GetMulti(int max=7) {
+            return Enumerable.Range(1, max).Select(index => new WeatherForecast {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ Day10Lab1b/Program.cs
@@ -0,0 +1,28 @@
+﻿using Day10Lab1;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Press Return to start!");
+
+Console.ReadLine();
+
+HttpClient MyCn = new HttpClient();
+string URIBASE = "http://localhost:5152/WeatherForecast";
+
+HttpResponseMessage response = await MyCn.GetAsync(URIBASE);
+if(response.IsSuccessStatusCode) {
+    //Ok
+    WeatherForecast[] Antani = await response.Content.ReadFromJsonAsync<WeatherForecast[]>();
+    foreach(var w in Antani) {
+        Console.WriteLine("-----------  Weather Result -----------");
+        Console.WriteLine($"{w.Date.ToLocalTime()} temp {w.TemperatureC}° ");
+    }
+    //EOW
+} else {
+    Console.WriteLine($"Sorry: {response.StatusCode}: {response.ReasonPhrase}");
+}
+
+Console.ReadLine();
