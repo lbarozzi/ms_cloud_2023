7c2e3cb2185dea9b5e80382837223ac560301427
Leonardo Barozzi <leonardo.barozzi@mail.com>
2023-03-24 14:03:49 +0100

    Day13Lab2 Start

---
+++ Day13Lab2/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace Day13Lab2.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
