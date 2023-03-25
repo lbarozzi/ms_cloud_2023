668274cc246824d6edb6ad9b4669e1579ec592f2
Leonardo Barozzi <leonardo.barozzi@mail.com>
2023-03-25 08:45:02 +0100

    fix decimal issu dot or comma

--- Day13Lab2/Program.cs
+++ Day13Lab2/Program.cs
@@ -1,5 +1,6 @@
 using Day13Lab2.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +20,19 @@ if (!app.Environment.IsDevelopment()) {
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+//Bypass Validation dot or comma issue
+//https://stackoverflow.com/questions/48066208/mvc-jquery-validation-does-not-accept-comma-as-decimal-separator
+var supportedCultures = new[]
+{
+        new CultureInfo("en-US"),
+        new CultureInfo("es"),
+};
+
+app.UseRequestLocalization(new RequestLocalizationOptions { 
+    DefaultRequestCulture = new Microsoft.AspNetCore.Localization.RequestCulture("en-US"),
+    SupportedCultures = supportedCultures
+});
 
 
 app.UseHttpsRedirection();
