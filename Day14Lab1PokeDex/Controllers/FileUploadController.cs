61f0ad9bf0477adaa4a8dc7599a81858fb6bdda5
Leonardo Barozzi <leonardo.barozzi@mail.com>
2023-03-27 16:21:12 +0200

    Upload file 1

---
+++ Day14Lab1PokeDex/Controllers/FileUploadController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Day14Lab1PokeDex.Controllers {
+    public class FileUploadController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult UploadFile() {
+            if(Request.Form.Files.Count > 0) {
+                foreach(var file in Request.Form.Files) {
+                    var fileName = file.FileName;
+                    using(var outpuFile = System.IO.File.Create($"Upload/{fileName}")) { 
+                        file.CopyTo(outpuFile);
+                    }
+                }
+
+            }
+
+            return Ok("Saved!");
+        }
+    }
+}
