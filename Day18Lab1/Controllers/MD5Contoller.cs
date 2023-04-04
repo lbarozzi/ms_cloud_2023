2ad3c362cda5ef64acfb246986cdb962e1d8a3ca
Leonardo Barozzi <leonardo.barozzi@mail.com>
2023-04-04 11:00:52 +0200

    Day18Lab

---
+++ Day18Lab1/Controllers/MD5Contoller.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography;
+
+namespace Day18Lab1.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MD5Contoller : ControllerBase {
+
+        protected readonly System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create();
+        public MD5Contoller() {
+
+        }
+
+        protected string Compute(string PlainText) {
+            byte[] HashCode = md5.ComputeHash(System.Text.UTF8Encoding.UTF8.GetBytes(PlainText));
+            return BitConverter.ToString(HashCode);
+        }
+
+        [HttpGet]
+        public IActionResult Get(string PlainText) { 
+            return Ok(Compute(PlainText));
+        }
+
+        [HttpPost]
+        public IActionResult Validate(string PlainText,string HexText) {
+            //ff-aa-bb-99
+            /* Long and useless way
+            string[] tk = HexText.Split('-');
+            List<byte> tmpls = new List<byte>();
+            foreach(string s in tk) {
+                byte tmp;
+                if (Byte.TryParse(s, out tmp) {
+                    tmpls.Add(tmp);
+                }
+            }
+            byte[] bin_has= tmpls.ToArray<byte>();  
+            //*/
+            if (HexText.Equals(Compute(PlainText)))  { 
+                return Ok(true);
+            } else {
+                return Ok(false);
+            }
+            return BadRequest();
+        }
+    }
+}
---
+++ Day18Lab2/Program.cs
@@ -0,0 +1,65 @@
+using Day18Lab2.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddControllersWithViews();
+
+string uri = builder.Configuration["WebMD5:uri"];
+
+
+/* appsettings.json
+  "WebMD5": {
+    "uri": "https://localhost:7261/api/MD5Contoller"
+  },
+  "cats": {
+    "uri": "https://cat-fact.herokuapp.com/facts"
+  }
+//*/
+//
+builder.Services.AddHttpClient("Md5", client => {
+    client.BaseAddress = new Uri(uri) ;
+    client.DefaultRequestHeaders.Add("mytoken", "antanti");
+});
+
+//cats
+uri = builder.Configuration["cats:uri"];
+builder.Services.AddHttpClient("cats", client => {
+    client.BaseAddress = new Uri(uri);
+    client.DefaultRequestHeaders.Add("mytoken", "antanti");
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment()) {
+    app.UseMigrationsEndPoint();
+} else {
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+
+app.Run();
