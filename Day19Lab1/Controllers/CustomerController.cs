8e0937f897b691056c145ad13a42e8cca9ce42db
Leonardo Barozzi <leonardo.barozzi@mail.com>
2023-04-06 09:55:11 +0200

    partial ok

--- Day19Lab1/Controllers/CustomerController.cs
+++ Day19Lab1/Controllers/CustomerController.cs
@@ -7,6 +7,19 @@ namespace Day19Lab1.Controllers {
         private readonly DataContext _Context;
         public CustomerController(DataContext _DContext) {
             _Context = _DContext;
+            //Popolate DB
+            if (_Context.Customers.Count() == 0) {
+                for (int i = 0; i < 10; i++) {
+                    Customer mio = new Customer() {
+                        CusotmerStatus = 1,
+                        CustomerName = $"Customer {i}",
+                        CustomerEmail = $"cust{i}@test.com",
+                        CustomerPhone = $"555-5{i}9-9338"
+                    };
+                    _Context.Customers.Add(mio);
+                }
+                _Context.SaveChanges();
+            }
         }
         public IActionResult Index() {
             var custs= _Context.Customers.ToList();
